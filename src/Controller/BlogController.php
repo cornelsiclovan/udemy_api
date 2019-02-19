2e6cacc896c5c3f645a0ef1ed93d3a591e456375
cornel.siclovan <cornel.siclovan@gmail.com>
2019-02-19 15:21:34 +0200

    Starting doctrine fixtures seeding fake data

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -10,68 +10,90 @@ namespace App\Controller;
 
 
 
+use App\Entity\BlogPost;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Serializer;
 
 /**
  * @Route("/blog")
  */
 class BlogController extends AbstractController
 {
-    private const POSTS = [[
-            'id'    => 1,
-            'slug'  => 'hello-world',
-            'title' => 'Hello world!'
-        ],
-        [
-            'id'    => 2,
-            'slug'  => 'another-post',
-            'title' => 'Another post'
-        ],
-        [
-            'id'    => 3,
-            'slug'  => 'last-example',
-            'title' => 'Last example'
-        ],
-    ];
-
     /**
-     * @Route("/{page}", name="blog_list", defaults={"page": 1})
+     * @Route("/{page}", name="blog_list", defaults={"page": 1}, requirements={"page"="\d+"})
      */
     public function list($page, Request $request)
     {
         $limit = $request->get('limit', 10);
+        $repository = $this->getDoctrine()->getRepository(BlogPost::class);
+        $items = $repository->findAll();
+
+
         return $this->json(
             [
                 'page'  => $page,
                 'limit' => $limit,
-                'data'  => array_map(function($item){
-                    return $this->generateUrl(  'post_by_slug', ['slug' => $item['slug']]);
-                }, self::POSTS)
+                'data'  => array_map(function(BlogPost $item){
+                    return $this->generateUrl(  'post_by_slug', ['slug' => $item->getSlug()]);
+                }, $items)
             ]
         );
     }
 
     /**
-     * @Route("/{id}", name="post_by_id", requirements={"id"="\d+"})
+     * @Route("/post/{id}", name="post_by_id", requirements={"id"="\d+"}, methods={"GET"})
+     * @ParamConverter("post", class="App\Entity\BlogPost")
      */
-    public function post($id)
+    public function post($post)
     {
-        return new JsonResponse(
-            self::POSTS[array_search($id, array_column(self::POSTS, 'id'))]
+        return $this->json(
+          $post
         );
     }
 
     /**
-     * @Route("/{slug}", name="post_by_slug")
+     * @Route("/post/{slug}", name="post_by_slug", methods={"GET"})
+     * @ParamConverter("post", options={"mapping": {"slug": "slug"}}, class="App\Entity\BlogPost")
      */
-    public function postBySlug($slug)
+    public function postBySlug($post)
     {
         return $this->json(
-            self::POSTS[array_search($slug, array_column(self::POSTS, 'slug'))]
+            $post
         );
+    }
+
+    /**
+     * @Route("/add", name="blog_add", methods={"POST"})
+     */
+    public function add(Request $request)
+    {
+        /** @var Serializer $serializer */
+        $serializer = $this->get('serializer');
+
+        $blogPost = $serializer->deserialize($request->getContent(), BlogPost::class, 'json');
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($blogPost);
+        $em->flush();
+
+        return $this->json($blogPost);
+    }
+
+    /**
+     * @Route("/post/{id}", name="blog_delete", methods={"DELETE"})
+     */
+    public function delete(BlogPost $post)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($post);
+        $em->flush();
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 
 }
